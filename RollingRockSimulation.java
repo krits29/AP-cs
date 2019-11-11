39346b1c5378e93a084e5aa325c43beb1517fa9c
krits29 <31025768+krits29@users.noreply.github.com>
2019-11-10 22:54:19 -0800

    Update RollingRockSimulation.java

--- RollingRockSimulation.java
+++ RollingRockSimulation.java
@@ -4,8 +4,15 @@
    11/7/2019
    Period 3
    hi -andrea from github
+   hi - kriti from github
 */
 
 import java.util.Random;
 
 public class RollingRockSimulation
+{
+   public static void main(String[] args)
+   {
+      
+   }
+}
