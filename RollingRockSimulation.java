11f983584b64b531ed45f8753ad8545588baa5de
krits29 <31025768+krits29@users.noreply.github.com>
2019-11-10 18:27:44 -0800

    Update RollingRockSimulation.java

--- RollingRockSimulation.java
+++ RollingRockSimulation.java
@@ -8,3 +8,16 @@
 import java.util.Random;
 
 public class RollingRockSimulation
+{
+   public static void main(String[] args)
+   {
+      Random gen = new Random(); 
+   }
+}
+   
+   
+   
+   
+   
+   // okay we can write the sudo code here
+   // and we can have an excessive amount of comments througout the code so that both of us know whats going on lol
